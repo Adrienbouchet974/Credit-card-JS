c1f0987ae355e15a84efa7812ff2638556e13e84
Adrien bouchet <adrien.bouchet.974@gmail.com>
2022-06-09 12:26:58 +0200

    Ajout de whitespace pour la lisibilité.

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,23 +12,25 @@ const input_expiration = document.querySelector("#Expiration");
 const card_numero = document.querySelector("#Num_card");
 const card_nom = document.querySelector("#card_nom");
 const card_prenom = document.querySelector("#card_prenom");
-const card_expiration = document.querySelector("#card_expiration")
+const card_expiration = document.querySelector("#card_expiration");
 
 
 input.addEventListener("submit", function(event) {
     event.preventDefault();
+
     const value_card = input_number.value;
     const value_last_name = input_last_name.value;
     const value_first_name = input_first_name.value;
     const value_expiration = input_expiration.value;
-
+    
     console.log(value_card);
     console.log(value_last_name);
     console.log(value_first_name);
     console.log(value_expiration);
-
+    
     card_numero.textContent += ` ${value_card}`;
     card_nom.textContent += ` ${value_last_name}`;
     card_prenom.textContent += ` ${value_first_name}`;
     card_expiration.textContent += ` ${value_expiration}`;
-})
+
+    })
