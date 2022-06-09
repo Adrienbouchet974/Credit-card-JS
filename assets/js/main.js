a0596fcdfd8e999fc38f2cf8964ff35ab713409a
Adrien bouchet <adrien.bouchet.974@gmail.com>
2022-06-09 11:30:48 +0200

    Pour récupérer les valeurs des inputs.

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+const input = document.querySelector("#form");
+console.log(input);
+
+const input_number = document.querySelector("#number");
+const input_last_name = document.querySelector("#Nom");
+const input_first_name = document.querySelector("#Prénom");
+
+
+input.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const value_card = input_number.value;
+    const value_last_name = input_last_name.value;
+    const value_first_name = input_first_name.value;
+
+    console.log(value_card);
+    console.log(value_last_name);
+    console.log(value_first_name);
+})
+// input_last_name.addEventListener("submit", function(e) {
+//     e.preventDefault();
+//     console.log(e);
+// })
+// input_first_name.addEventListener("submit", function(e) {
+//     e.preventDefault();
+//     console.log(e);
+// })
+
+
+// const card_number = document.querySelector('');
