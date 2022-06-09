b1986fb18d1c56590d343136cb8f1a34f5ca0b13
Adrien bouchet <adrien.bouchet.974@gmail.com>
2022-06-09 16:21:51 +0200

    Ajout de regex et de conditions.

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,35 +7,72 @@ const input_number = document.querySelector("#number");
 const input_last_name = document.querySelector("#Nom");
 const input_first_name = document.querySelector("#Prenom");
 const input_expiration = document.querySelector("#Expiration");
+const input_crypto = document.querySelector("#crypto");
 
 //pour récupérer le texte
 const card_numero = document.querySelector("#Num_card");
 const card_nom = document.querySelector("#card_nom");
 const card_prenom = document.querySelector("#card_prenom");
 const card_expiration = document.querySelector("#card_expiration");
+const card_crypto = document.querySelector("#cryptogramme");
 
 
 input.addEventListener("submit", function(event) {
     event.preventDefault();
-
+    
     const value_card = input_number.value;
     const value_last_name = input_last_name.value;
     const value_first_name = input_first_name.value;
     const value_expiration = input_expiration.value;
+    // const value_crypto = input_crypto.value;
+
+    const card_length = value_card.length;
+    console.log(card_length);
+    const expiration_length = value_expiration.length;
+    console.log(expiration_length);
+
+    if(card_length == 16 && value_card.match(/([0-9]{16})/)){
+
+        card_numero.textContent += ` ${value_card}`;
+        
+    }else{
+        alert("Votre numéro de carte bancaire doit posséder 16 chiffres");
+    }
+
+    if(value_last_name.match(/\w/)){
+
+        card_nom.textContent += ` ${value_last_name}`;
+        
+    }else{
+        alert("Votre nom ne doit pas comporter de nombres");
+    }
+
+    if(value_first_name.match(/\w/)){
+
+        card_prenom.textContent += ` ${value_first_name}`;
+        
+    }else{
+        alert("Votre prénom ne doit pas comporter de nombres");
+    }
+
+    if(expiration_length == 5 && value_expiration.match(/([0-9]{2})[/]([0-9]{2})/)){
+
+        card_expiration.textContent += ` ${value_expiration}`;
+        
+    }else{
+        alert("le format de la date est une série de 2 nombres séparée par un /. Exemple : 14/96");
+    }
     
-    console.log(value_card);
-    console.log(value_last_name);
-    console.log(value_first_name);
-    console.log(value_expiration);
-    
-    card_numero.textContent += ` ${value_card}`;
-    card_nom.textContent += ` ${value_last_name}`;
-    card_prenom.textContent += ` ${value_first_name}`;
-    card_expiration.textContent += ` ${value_expiration}`;
 
 });
 
 const credit_card = document.querySelector("#credit_card");
+const info_0 = document.querySelector("#Num_card");
+const info_1 = document.querySelector("#card_nom");
+const info_2 = document.querySelector("#card_prenom");
+const info_3 = document.querySelector("#card_expiration");
+
+
 console.log(credit_card);
 
 credit_card.addEventListener("click", function(){
